9b4242228a130c156adf4fe16a91b2bbe2d88833
Fanicuki <faygamer321@gmail.com>
2024-08-14 19:31:26 -0300

    form login initiation

---
+++ login.svelte
@@ -0,0 +1,65 @@
+<script>
+
+</script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+<div class="contenedor">
+    <div class="content">
+        <form>
+                <svg width="46" height="46" fill="none" stroke="orange" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M22 12a9.969 9.969 0 0 1-2.944 7.087A9.968 9.968 0 0 1 12 22C6.477 22 2 17.523 2 12a9.966 9.966 0 0 1 2.75-6.888A9.972 9.972 0 0 1 12 2a9.969 9.969 0 0 1 7.056 2.913A9.97 9.97 0 0 1 22 12Z"></path>
+                    <path d="M22 12c-1.459 0-5.484-.55-9.087 1.031C9 14.75 6.166 17.416 4.932 19.073"></path>
+                    <path d="M8.25 2.727C9.815 4.17 13.23 7.849 14.5 11.5c1.27 3.65 1.74 8.14 2.03 9.417"></path>
+                    <path d="M2.077 10.75c1.889.114 6.89.217 10.09-1.15 3.2-1.366 5.953-3.88 6.897-4.677"></path>
+                    <path d="M2.75 15.807a10.038 10.038 0 0 0 4.5 4.995"></path>
+                    <path d="M2 12a9.966 9.966 0 0 1 2.75-6.888"></path>
+                    <path d="M12 2a9.972 9.972 0 0 0-7.25 3.112"></path>
+                    <path d="M16 2.832a10.02 10.02 0 0 1 3.056 2.081A9.97 9.97 0 0 1 22 12a9.98 9.98 0 0 1-.63 3.5"></path>
+                    <path d="M12 22a9.968 9.968 0 0 0 7.056-2.913"></path>
+                  </svg>
+                DeportiNet
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Email address</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+              <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputPassword1" class="form-label">Password</label>
+              <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3 form-check">
+              <input type="checkbox" class="form-check-input" id="exampleCheck1">
+              <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+    </div>
+    
+</div>
+
+
+<style>
+
+.contenedor {
+    background-color: #244a9c;
+    width: 100%;
+    height: 100%;
+}
+
+.content {
+    position: relative;
+    background-color: #3165d4;
+    max-width: 350px;
+    border: 3px solid orange;
+    border-radius: 10px;
+}
+
+.content form {
+    margin: 30px;
+}
+
+form label {
+    color: white;
+}
+
+</style>
