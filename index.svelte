e42d2fabd92a7f7f7fd7cc50421bc86d50909615
Fanicuki <faygamer321@gmail.com>
2024-08-13 21:37:56 -0300

    reformulación de frontend menú

--- index.svelte
+++ index.svelte
@@ -9,7 +9,7 @@
     <nav class="navbar bg-body-tertiary">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">
-            <svg width="46" height="46" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <svg width="46" height="46" fill="none" stroke="orange" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path d="M22 12a9.969 9.969 0 0 1-2.944 7.087A9.968 9.968 0 0 1 12 22C6.477 22 2 17.523 2 12a9.966 9.966 0 0 1 2.75-6.888A9.972 9.972 0 0 1 12 2a9.969 9.969 0 0 1 7.056 2.913A9.97 9.97 0 0 1 22 12Z"></path>
                 <path d="M22 12c-1.459 0-5.484-.55-9.087 1.031C9 14.75 6.166 17.416 4.932 19.073"></path>
                 <path d="M8.25 2.727C9.815 4.17 13.23 7.849 14.5 11.5c1.27 3.65 1.74 8.14 2.03 9.417"></path>
@@ -22,6 +22,18 @@
               </svg>
             DeportiNet
           </a>
+            <div class="navbar-icons">
+            <a class="shopping-cart" type="button" href=""><svg width="30" height="30" fill="none" stroke="orange" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path d="M9 20a1 1 0 1 0 0 2 1 1 0 1 0 0-2z"></path>
+                <path d="M20 20a1 1 0 1 0 0 2 1 1 0 1 0 0-2z"></path>
+                <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+            </svg></a>
+            <a class="profile" type="button" href=""><svg width="46" height="46" fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+              <path d="M12 3a4 4 0 1 0 0 8 4 4 0 1 0 0-8z"></path>
+            </svg></a>
+          
+            </div>
         </div>
     </nav>
     <div class="sidebar">
@@ -29,7 +41,9 @@
         <a href="#tutorials">Catalogo</a>
     </div>
     <div class="content">
-
+        <div>
+            
+        </div>
     </div>
 </div>
 <style>
@@ -38,6 +52,27 @@
     --bs-gutter-x: 0rem;
 }
 
+.bg-body-tertiary {
+    background-color: #3165d4 !important;
+}
+
+.navbar-icons {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 30px;
+    align-items: center;
+}
+
+.profile {
+    margin-right: 20px;
+}
+
+.navbar-brand {
+    color: #fff;
+}
+.navbar-brand a:hover {
+    color: orange;
+}
 .content {
     position: fixed;
     left: 200px;
@@ -46,13 +81,13 @@
 }
 
 .content {
-    background-color: #f3f3f3;
+    background-color: #ffffff;
 }
 
 .sidebar {
     width: 200px;
     height: 100%;
-    background-color: #2b2b2b;
+    background-color: #3165d4e0;
     color: #fff;
     box-sizing: border-box;
     position: fixed;
@@ -66,6 +101,6 @@
 }
 
 .sidebar a:hover {
-    background-color: #2980b9;
+    background-color: orange;
 }
 </style>
