8336c73f60c75a1d619b8660bb2e8cf3bf1b07b6
Fanicuki <faygamer321@gmail.com>
2024-08-14 21:25:02 -0300

    login web fronted finish

--- login.svelte
+++ login.svelte
@@ -8,87 +8,55 @@
 
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
-<!-- <div class="contenedor">
-    <div class="content">
-        <form>
-                <svg width="46" height="46" fill="none" stroke="orange" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M22 12a9.969 9.969 0 0 1-2.944 7.087A9.968 9.968 0 0 1 12 22C6.477 22 2 17.523 2 12a9.966 9.966 0 0 1 2.75-6.888A9.972 9.972 0 0 1 12 2a9.969 9.969 0 0 1 7.056 2.913A9.97 9.97 0 0 1 22 12Z"></path>
-                    <path d="M22 12c-1.459 0-5.484-.55-9.087 1.031C9 14.75 6.166 17.416 4.932 19.073"></path>
-                    <path d="M8.25 2.727C9.815 4.17 13.23 7.849 14.5 11.5c1.27 3.65 1.74 8.14 2.03 9.417"></path>
-                    <path d="M2.077 10.75c1.889.114 6.89.217 10.09-1.15 3.2-1.366 5.953-3.88 6.897-4.677"></path>
-                    <path d="M2.75 15.807a10.038 10.038 0 0 0 4.5 4.995"></path>
-                    <path d="M2 12a9.966 9.966 0 0 1 2.75-6.888"></path>
-                    <path d="M12 2a9.972 9.972 0 0 0-7.25 3.112"></path>
-                    <path d="M16 2.832a10.02 10.02 0 0 1 3.056 2.081A9.97 9.97 0 0 1 22 12a9.98 9.98 0 0 1-.63 3.5"></path>
-                    <path d="M12 22a9.968 9.968 0 0 0 7.056-2.913"></path>
-                  </svg>
-                DeportiNet
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">Email address</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-              <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-            </div>
-            <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label">Password</label>
-              <input type="password" class="form-control" id="exampleInputPassword1">
-            </div>
-            <div class="mb-3 form-check">
-              <input type="checkbox" class="form-check-input" id="exampleCheck1">
-              <label class="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-          </form>
+<div class="contenedor">
+    <div class="logo-container">
+        <svg width="65" height="65" fill="none" stroke="orange" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M22 12a9.969 9.969 0 0 1-2.944 7.087A9.968 9.968 0 0 1 12 22C6.477 22 2 17.523 2 12a9.966 9.966 0 0 1 2.75-6.888A9.972 9.972 0 0 1 12 2a9.969 9.969 0 0 1 7.056 2.913A9.97 9.97 0 0 1 22 12Z"></path>
+            <path d="M22 12c-1.459 0-5.484-.55-9.087 1.031C9 14.75 6.166 17.416 4.932 19.073"></path>
+            <path d="M8.25 2.727C9.815 4.17 13.23 7.849 14.5 11.5c1.27 3.65 1.74 8.14 2.03 9.417"></path>
+            <path d="M2.077 10.75c1.889.114 6.89.217 10.09-1.15 3.2-1.366 5.953-3.88 6.897-4.677"></path>
+            <path d="M2.75 15.807a10.038 10.038 0 0 0 4.5 4.995"></path>
+            <path d="M2 12a9.966 9.966 0 0 1 2.75-6.888"></path>
+            <path d="M12 2a9.972 9.972 0 0 0-7.25 3.112"></path>
+            <path d="M16 2.832a10.02 10.02 0 0 1 3.056 2.081A9.97 9.97 0 0 1 22 12a9.98 9.98 0 0 1-.63 3.5"></path>
+            <path d="M12 22a9.968 9.968 0 0 0 7.056-2.913"></path>
+          </svg>
+        DeportiNet
     </div>
-    
-</div> -->
+    <div class="main">
 
-<div class="main">  	
-    <input type="checkbox" id="chk" aria-hidden="true">
+        <input type="checkbox" id="chk" aria-hidden="true">
 
-        <div class="login">
-            <form>
-                <label for="chk" aria-hidden="true">Iniciar Sesión</label>
-                <input type="email" name="email" placeholder="Usuario" required="">
-                <input type="password" name="pswd" placeholder="Contraseña" required="">
-                <button>Entrar</button>
-            </form>
-        </div>
+            <div class="login">
+                <form>
+                    <label for="chk" aria-hidden="true">Iniciar Sesión</label>
+                    <input type="email" name="email" placeholder="Usuario" required="">
+                    <input type="password" name="pswd" placeholder="Contraseña" required="">
+                    <button>Entrar</button>
+                </form>
+            </div>
 
-        <div class="signup">
-            <form>
-                <label for="chk" aria-hidden="true">Crear Usuario</label>
-                <input type="text" name="txt" placeholder="Usuario" required="">
-                <input type="email" name="email" placeholder="Email" required="">
-                <input type="password" name="pswd" placeholder="Contraseña" required="">
-                <button>Registrarse</button>
-            </form>
-        </div>
+            <div class="signup">
+                <form>
+                    <label for="chk" aria-hidden="true">Crear Usuario</label>
+                    <input type="text" name="txt" placeholder="Usuario" required="">
+                    <input type="email" name="email" placeholder="Email" required="">
+                    <input type="password" name="pswd" placeholder="Contraseña" required="">
+                    <button>Registrarse</button>
+                </form>
+            </div>
+    </div>
 </div>
 
 
 <style>
 
-/* .contenedor {
-    background-color: #244a9c;
-    width: 100%;
-    height: 100%;
-}
-
-.content {
+.logo-container {
     position: relative;
-    background-color: #3165d4;
-    max-width: 350px;
-    border: 3px solid orange;
-    border-radius: 10px;
-}
-
-.content form {
-    margin: 30px;
-}
-
-form label {
     color: white;
-} */
+    font-size: 50px;
+    z-index: 1;
+}
 
 body{
 	margin: 0;
@@ -98,7 +66,7 @@ body{
 	align-items: center;
 	min-height: 100vh;
 	font-family: 'Jost', sans-serif;
-	background: linear-gradient(to bottom, #0f0c29, #302b63, #24243e);
+	background: linear-gradient(to bottom, #162c47, #436096, #242c3e);
 }
 .main{
 	width: 350px;
@@ -108,6 +76,7 @@ body{
 	border-radius: 10px;
 	box-shadow: 5px 20px 50px #000;
 }
+
 #chk{
 	display: none;
 }
